a50d74f273d85594f298eaa0bcea2044a816ee88
Piyush Sindhu <piyush124790@gmail.com>
2025-09-07 19:27:19 +0530

    Day 6 of C Programming
    Write a Program to Input an Integer and Check Whether it is Even or Odd Using If-Else Statement.

---
+++ Day_6.c
@@ -0,0 +1,18 @@
+// Write a Program to Input an Integer and Check Whether it is Even or Odd Using If-Else Statement
+
+#include <stdio.h>
+
+int main(){
+
+    int num;
+    printf("Enter an integer: ");
+    scanf("%d", &num);
+
+    if(num % 2 == 0){
+        printf("%d is Even\n", num);
+    } else {
+        printf("%d is Odd\n", num);
+    }
+
+    return 0;
+}
