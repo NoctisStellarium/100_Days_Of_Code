a70f703a5b6a55d9cf1ec49d7cf35b9196b28bec
Piyush Sindhu <piyush124790@gmail.com>
2025-09-07 23:09:36 +0530

    Day 7 of C Programming
    Write a Program to Input a Year and Check Whether it is a Leap Year or Not Using Conditional Statements.

---
+++ Day_7.c
@@ -0,0 +1,18 @@
+// Write a Program to Input a Year and Check Whether it is a Leap Year or Not Using Conditional Statements
+
+#include <stdio.h>
+
+int main() {
+    
+    int year;
+    printf("Enter a Year: ");
+    scanf("%d", &year);
+
+    if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)) {
+        printf("%d is a Leap ear.\n", year);
+    } else {
+        printf("%d is Not a Leap Year.\n", year);
+    }
+
+    return 0;
+}
