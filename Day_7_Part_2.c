9532a3eec5cff893ed6d0e15c20e619eacb1f60e
Piyush Sindhu <piyush124790@gmail.com>
2025-09-07 23:05:39 +0530

    Day 7 of C Programming
    Write a Program to Input a Character and Check Whether it is a Vowel or Consonant using If-Else Statement.

--- Day_7_Part_2.c
+++ Day_7_Part_2.c
@@ -6,18 +6,18 @@
 int main(){
 
     char ch;
-    printf("Enter a character: ");
+    printf("Enter a Character: ");
     scanf("%c", &ch);
 
     if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u' ||
         ch == 'A' || ch == 'E' || ch == 'I' || ch == 'O' || ch == 'U') {
-        printf("%c is a vowel.\n", ch);
+        printf("%c is a Vowel.\n", ch);
     }
     else if ((ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch <= 'Z')) {
-        printf("%c is a consonant.\n", ch);
+        printf("%c is a Consonant.\n", ch);
     }
     else {
-        printf("%c is not an alphabetic character.\n", ch);
+        printf("%c is not an Alphabetic Character.\n", ch);
     }
 
     return 0;
