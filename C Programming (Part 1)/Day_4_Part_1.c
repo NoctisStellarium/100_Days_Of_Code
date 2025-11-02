361e23c0c4349b3acd678cf8bd84367cd3ac231a
Piyush Sindhu <piyush124790@gmail.com>
2025-11-02 23:02:18 +0530

    Day 4 Of C Programming
    Write a Program to Swap Two Numbers Without Using a Third Variable.

---
+++ C Programming (Part 1)/Day_4_Part_1.c
@@ -0,0 +1,27 @@
+// Write a Program to Swap Two Numbers Without Using a Third Variable.
+
+#include <stdio.h> 
+
+int main()
+{
+    int num1;
+    int num2;
+
+    printf("Please Enter The First Number : ");
+    scanf("%d", &num1);
+
+    printf("Please Enter The Second Number : ");
+    scanf("%d", &num2);
+
+    printf("Before Swap\n");
+    printf("The First Number Is %d & The Second Number Is %d\n", num1, num2);
+
+    num1 = num1 + num2;
+    num2 = num1 - num2;
+    num1 = num1 - num2;
+    
+    printf("After Swap\n");
+    printf("The First Number Is %d & The Second Number Is %d", num1, num2);
+
+    return 0;
+}
