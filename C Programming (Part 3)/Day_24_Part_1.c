84f693a0960b0691baa45c2beefc23df6402b36b
Piyush Sindhu <piyush124790@gmail.com>
2025-11-05 16:18:22 +0530

    Day 24 Of C Programming
    Write a Program To Print The Following Pattern.

---
+++ C Programming (Part 3)/Day_24_Part_1.c
@@ -0,0 +1,23 @@
+// Write a Program To Print The Following Pattern.
+
+#include <stdio.h>
+
+int main()
+{
+    int n;
+
+    printf("Please Enter The Number Of Rows : ");
+    scanf("%d", &n);
+
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= i; j++)
+        {
+            printf("*");
+        }
+        
+        printf("\n");
+    }
+
+    return 0;
+}
